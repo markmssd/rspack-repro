6ee345a59536b7858ee627cd7d71a46b607d9a95
Mark Massoud <mark.massoud@unity3d.com>
2025-06-27 07:59:26 -0400

    use both html + rspack configs

--- rspack.config.mjs
+++ rspack.config.mjs
@@ -1,6 +1,7 @@
 import path from "path";
 import { fileURLToPath } from "url";
 import rspack from "@rspack/core";
+import htmlWebpackPlugin from "html-webpack-plugin";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const isRunningWebpack = !!process.env.WEBPACK;
@@ -18,15 +19,30 @@ const config = {
   entry: {
     main: "./src/index",
   },
-  plugins: [new rspack.HtmlRspackPlugin({
-    favicon: 'favicon.ico',
-    filename: 'index.ejs',
-    template: 'src/index.ejs',
-    templateParameters: {
-      title: '<%= title %>',
-    },
-    inject: 'body',
-  }),],
+  plugins: [
+    // this works, unescapedTitle will remain unescaped
+    new htmlWebpackPlugin({
+      favicon: 'favicon.ico',
+      filename: 'html.index.ejs',
+      template: 'src/index.ejs',
+      templateParameters: {
+        escapedTitle: '<%- escapedTitle %>',
+        unescapedTitle: '<%= unescapedTitle %>',
+      },
+      inject: 'body',
+    }),
+    // this doesn't works, unescapedTitle will be escaped
+    new rspack.HtmlRspackPlugin({
+      favicon: 'favicon.ico',
+      filename: 'rspack.index.ejs',
+      template: 'src/index.ejs',
+      templateParameters: {
+        escapedTitle: '<%- escapedTitle %>',
+        unescapedTitle: '<%= unescapedTitle %>',
+      },
+      inject: 'body',
+    }),
+  ],
   output: {
     clean: true,
     path: isRunningWebpack
